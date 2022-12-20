cd770ca41db0724663f41d0124a052dc35d18906
Anand <hyeiamaka06@gmail.com>
2022-12-20 23:15:38 +0530

    calendar

--- script.js
+++ script.js
@@ -12,11 +12,11 @@ var month_names = ['January',
     'October',
     'November',
     'December']
-    
-    var year_names = [];
-    for (var i = 1950; i <= 2072; i++) {
-        year_names.push(i);
-    }
+
+var year_names = [];
+for (var i = 1950; i <= 2072; i++) {
+    year_names.push(i);
+}
 
 function renderDate() {  // when body load  //current month & year->day sun-->0 to sat->1,similrly  month-->0 to 11
 
@@ -35,8 +35,8 @@ function renderDate() {  // when body load  //current month & year->day sun-->0 
     dt.setDate(1);
     var day = dt.getDay(); // first day of month
 
-// generating days
-    var count=42;
+    // generating days
+    var count = 42;
     var cells = "";
     for (var x = day; x > 0; x--) {
         count--;
@@ -50,7 +50,7 @@ function renderDate() {  // when body load  //current month & year->day sun-->0 
             cells += "<div>" + i + "</div>";
         }
     }
-    for (var y = endDaysOfMonth; y < count+endDaysOfMonth; y++) {
+    for (var y = endDaysOfMonth; y < count + endDaysOfMonth; y++) {
         cells += "<div class='prev-day'>" + (y - endDaysOfMonth + 1) + "</div>";
     }
     document.getElementsByClassName("calendar-days")[0].innerHTML = cells;
@@ -102,25 +102,11 @@ year_names.forEach((e, index) => {
     }
     year_list.appendChild(years)
 })
-// var p=0;
-// for(var z=1950;z<=2051;z++){
-//     let years = document.createElement('div')
-//     years.innerHTML = `<div data-year="${p++}">${z}</div>`
-//     years.querySelector('div').onclick = () => {
-//         year_list.classList.remove('show')
-//         dt.setFullYear(z);
-//         renderDate();
-//     }
-//     year_list.appendChild(years)
-// }
-
 let year_picker = document.querySelector('#year')
 
 year_picker.onclick = () => {
     year_list.classList.add('show')
 }
-
-
 // dark Mode code
 let dark_mode_toggle = document.querySelector('.dark-mode-switch')
 dark_mode_toggle.onclick = () => {
