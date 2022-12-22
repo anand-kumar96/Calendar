7153fd3a64fbac8a5e8301703ed1871b7c16fb5a
Anand <hyeiamaka06@gmail.com>
2022-12-22 23:48:46 +0530

    comment change

--- script.js
+++ script.js
@@ -1,47 +1,28 @@
-
 var dt = new Date();
-var month_names = ['January', // month list  
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December']
-
-var year_names = []; // range of year
-for (var i = 1950; i <= 2072; i++) {
+// month list 
+var month_names = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+var year_names = []; 
+for (var i = 1950; i <= 2072; i++) {  // range of year
     year_names.push(i);
 }
 
 function renderDate() {  //render function  when body load 
     document.querySelector('#month-picker').innerHTML = month_names[dt.getMonth()];
     document.querySelector('.year').innerHTML = dt.getFullYear();
-
-    // to print whole calendar 
-    var endDateOfMonth = new Date(dt.getFullYear(), dt.getMonth() + 1, 0).getDate();   // end Date of current month
-
-    var endDatePrevMonth = new Date(dt.getFullYear(), dt.getMonth(), 0).getDate();     // end date of previous month
+    var endDateOfMonth = new Date(dt.getFullYear(), dt.getMonth() + 1, 0).getDate(); //end Date of current month
+    var endDatePrevMonth = new Date(dt.getFullYear(), dt.getMonth(), 0).getDate();   //end date of previous month
     var endDaysOfMonth = new Date(dt.getFullYear(), dt.getMonth(), endDateOfMonth).getDay(); //end days of current month
-
-
     var today = new Date();
-
     dt.setDate(1);
-    var day = dt.getDay(); // first day of month
-
+    var day = dt.getDay(); //first day of month
     // generating days
-    var count = 42;  // to fix 6 row
+    var count = 42;  //to fix 6 row
     var cells = "";
-    for (var x = day; x > 0; x--) {   // prev month date
+    for (var x = day; x > 0; x--) {   //prev month date
         count--;
         cells += "<div class='prev-day'>" + (endDatePrevMonth - x + 1) + "</div>";
     }
-    for (var i = 1; i <= endDateOfMonth; i++) {   // current date
+    for (var i = 1; i <= endDateOfMonth; i++) {   //current date
         count--;
         if (i == today.getDate() && dt.getMonth() == today.getMonth() && dt.getFullYear() == today.getFullYear()) {
             cells += "<div class='today'>" + i + "</div>";
@@ -53,7 +34,6 @@ function renderDate() {  //render function  when body load
         cells += "<div class='prev-day'>" + (y - endDaysOfMonth + 1) + "</div>";
     }
     document.getElementsByClassName("calendar-days")[0].innerHTML = cells;
-
 }
 
 // next and prev month code
@@ -63,12 +43,14 @@ function moveMonth(para) {
     } else {
         dt.setMonth(dt.getMonth() + 1);
     }
+    if (dt.getFullYear() > 2072 || dt.getFullYear() < 1950) {
+        return;
+    }
     renderDate();
 }
 
 // dropdown list of month
 let month_list = document.querySelector('.month-list')
-
 month_names.forEach((e, index) => {
     let month = document.createElement('div')
     month.innerHTML = `<div data-month="${index}">${e}</div>`
@@ -81,16 +63,12 @@ month_names.forEach((e, index) => {
 })
 
 let month_picker = document.querySelector('#month-picker')
-
 month_picker.onclick = () => {
     month_list.classList.add('show')
 }
 
-
 // dropdown list of year
-
 let year_list = document.querySelector('.year-list')
-
 year_names.forEach((e, index) => {
     let years = document.createElement('div')
     years.innerHTML = `<div data-year="${index}">${e}</div>`
@@ -101,8 +79,8 @@ year_names.forEach((e, index) => {
     }
     year_list.appendChild(years)
 })
-let year_picker = document.querySelector('#year')
 
+let year_picker = document.querySelector('#year')
 year_picker.onclick = () => {
     year_list.classList.add('show')
 }
