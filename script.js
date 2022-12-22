2f647adeea23ba765424da683db00e139b141017
Anand <hyeiamaka06@gmail.com>
2022-12-22 11:59:03 +0530

    calender comment change

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
-var dt = new Date(); // I used without assign for global
-var month_names = ['January',
+var dt = new Date();
+var month_names = ['January', // month list  
     'February',
     'March',
     'April',
@@ -13,13 +13,12 @@ var month_names = ['January',
     'November',
     'December']
 
-var year_names = [];
+var year_names = []; // range of year
 for (var i = 1950; i <= 2072; i++) {
     year_names.push(i);
 }
 
-function renderDate() {  // when body load  //current month & year->day sun-->0 to sat->1,similrly  month-->0 to 11
-
+function renderDate() {  //render function  when body load 
     document.querySelector('#month-picker').innerHTML = month_names[dt.getMonth()];
     document.querySelector('.year').innerHTML = dt.getFullYear();
 
@@ -36,13 +35,13 @@ function renderDate() {  // when body load  //current month & year->day sun-->0 
     var day = dt.getDay(); // first day of month
 
     // generating days
-    var count = 42;
+    var count = 42;  // to fix 6 row
     var cells = "";
-    for (var x = day; x > 0; x--) {
+    for (var x = day; x > 0; x--) {   // prev month date
         count--;
         cells += "<div class='prev-day'>" + (endDatePrevMonth - x + 1) + "</div>";
     }
-    for (var i = 1; i <= endDateOfMonth; i++) {
+    for (var i = 1; i <= endDateOfMonth; i++) {   // current date
         count--;
         if (i == today.getDate() && dt.getMonth() == today.getMonth() && dt.getFullYear() == today.getFullYear()) {
             cells += "<div class='today'>" + i + "</div>";
@@ -50,7 +49,7 @@ function renderDate() {  // when body load  //current month & year->day sun-->0 
             cells += "<div>" + i + "</div>";
         }
     }
-    for (var y = endDaysOfMonth; y < count + endDaysOfMonth; y++) {
+    for (var y = endDaysOfMonth; y < count + endDaysOfMonth; y++) {   // next month date
         cells += "<div class='prev-day'>" + (y - endDaysOfMonth + 1) + "</div>";
     }
     document.getElementsByClassName("calendar-days")[0].innerHTML = cells;
@@ -107,6 +106,7 @@ let year_picker = document.querySelector('#year')
 year_picker.onclick = () => {
     year_list.classList.add('show')
 }
+
 // dark Mode code
 let dark_mode_toggle = document.querySelector('.dark-mode-switch')
 dark_mode_toggle.onclick = () => {
