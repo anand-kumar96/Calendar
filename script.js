69471bb6e69b6636e465503150d67e3ea11f1689
Anand <hyeiamaka06@gmail.com>
2022-12-27 02:24:33 +0530

    calendar

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-var dt = new Date();
-var d = new Date();
+var currentDate = new Date();
+
 // month list 
-var month_names = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-var year_names = [];
+var month_Names = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+var year_Names = [];
 for (var i = 1950; i <= 2072; i++) {  // range of year
-    year_names.push(i);
+    year_Names.push(i);
 }
 
 //render function  when body load 
 function renderDate() {
-    document.querySelector('#month-picker').innerHTML = month_names[dt.getMonth()];
-    document.querySelector('.year').innerHTML = dt.getFullYear();
-    var endDateOfMonth = new Date(dt.getFullYear(), dt.getMonth() + 1, 0).getDate(); //end Date of current month
-    var endDatePrevMonth = new Date(dt.getFullYear(), dt.getMonth(), 0).getDate();   //end date of previous month
-    var endDaysOfMonth = new Date(dt.getFullYear(), dt.getMonth(), endDateOfMonth).getDay(); //end days of current month
+    document.querySelector('#month-picker').innerHTML = month_Names[currentDate.getMonth()];
+    document.querySelector('.year').innerHTML = currentDate.getFullYear();
+    var endDateOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0).getDate(); //end Date of current month
+    var endDatePrevMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 0).getDate();   //end date of previous month
+    var endDaysOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), endDateOfMonth).getDay(); //end days of current month
     var today = new Date();
-    dt.setDate(1);
-    var day = dt.getDay(); //first day of month
+    currentDate.setDate(1);
+    var day = currentDate.getDay(); //first day of month
 
     // generating days
-    var count = 42;  //total days 
+    var count = 42;  //(prev + current + next) month days
     var cells = "";
     for (var x = day; x > 0; x--) {   //prev month date
         count--;
@@ -27,7 +27,7 @@ function renderDate() {
     }
     for (var i = 1; i <= endDateOfMonth; i++) {   //current date
         count--;
-        if (i == today.getDate() && dt.getMonth() == today.getMonth() && dt.getFullYear() == today.getFullYear()) {
+        if (i == today.getDate() && currentDate.getMonth() == today.getMonth() && currentDate.getFullYear() == today.getFullYear()) {
             cells += "<div class='today'>" + i + "</div>";
         } else {
             cells += "<div>" + i + "</div>";
@@ -41,29 +41,42 @@ function renderDate() {
 
 // next and prev month code
 function moveMonth(para) {
-    if (para == 'prev' && dt.getFullYear() >= 1950) {
-        if (dt.getFullYear() == 1950 && dt.getMonth() == 0) { //to fix the range of prev year
-            dt.setMonth(dt.getMonth() + 1);
+    document.getElementById("next").style.cursor = "pointer";
+    document.getElementById("prev").style.cursor = "pointer";
+    if (para == 'prev') {
+        if (currentDate.getFullYear() == 1950 && currentDate.getMonth() == 1) {
+            document.getElementById("prev").style.cursor = "default";
         }
-        dt.setMonth(dt.getMonth() - 1);
+        if (currentDate.getFullYear() == 1950 && currentDate.getMonth() == 0) {
+            document.getElementById("prev").style.cursor = "default";
+            return;
+        } else {
+            currentDate.setMonth(currentDate.getMonth() - 1);
+        }
+    }
+    if (para == 'next') {
+        if (currentDate.getFullYear() == 2072 && currentDate.getMonth() == 10) {
+            document.getElementById("next").style.cursor = "default";
+        }
+        if (currentDate.getFullYear() == 2072 && currentDate.getMonth() == 11) {
+            document.getElementById("next").style.cursor = "default";
+            return;
+        } else {
+            currentDate.setMonth(currentDate.getMonth() + 1);
 
-    } else if (para == "next" && dt.getFullYear() <= 2072) {
-        if (dt.getFullYear() == 2072 && dt.getMonth() == 11) { //to fix the range of next year
-            dt.setMonth(dt.getMonth() - 1);
         }
-        dt.setMonth(dt.getMonth() + 1);
     }
     renderDate();
 }
 
 // dropdown list of month
 let month_list = document.querySelector('.month-list')
-month_names.forEach((e, index) => {
+month_Names.forEach((e, index) => {
     let month = document.createElement('div')
     month.innerHTML = `<div data-month="${index}">${e}</div>`
     month.querySelector('div').onclick = () => {
         month_list.classList.remove('show')
-        dt.setMonth(index);
+        currentDate.setMonth(index);
         renderDate();
     }
     month_list.appendChild(month)
@@ -76,12 +89,12 @@ month_picker.onclick = () => {
 
 // dropdown list of year
 let year_list = document.querySelector('.year-list')
-year_names.forEach((e, index) => {
+year_Names.forEach((e, index) => {
     let years = document.createElement('div')
     years.innerHTML = `<div data-year="${index}">${e}</div>`
     years.querySelector('div').onclick = () => {
         year_list.classList.remove('show')
-        dt.setFullYear(e);
+        currentDate.setFullYear(e);
         renderDate();
     }
     year_list.appendChild(years)
